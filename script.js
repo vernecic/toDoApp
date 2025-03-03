35daedf8370b1ce19054d5e808a3cc2c61eb5356
vernecic <vidernecic1@gmail.com>
2025-03-03 19:57:31 +0100

    No seperate done tasks page, some design changes.

--- script.js
+++ script.js
@@ -3,15 +3,14 @@ const tasklist = document.querySelector(".tasks");
 const inputField = document.getElementById("addTask");
 const buttonDoneTasks = document.querySelector(".doneTasks");
 const doneTasksContainer = document.querySelector(".doneTasksContainer");
-let allTodos = [];
 
-let doneTodos = [];
+let allTodos = [];
 
 buttonAdd.addEventListener("click", () => {
   const addedTask = inputField.value;
   if (!addedTask) return; // ne moze bit prazno
   console.log(addedTask);
-  const html = `<div class="newTask"><h4>${addedTask}</h4><div class="newTaskButtons"><img src="/images/done-round-svgrepo-com.svg" class="done"><img src="/images/delete-1487-svgrepo-com.svg" class="trashCan"></div></div>`;
+  const html = `<div class="newTask"><h4 class="newTaskName">${addedTask}</h4><div class="newTaskButtons"><img src="/images/done-round-svgrepo-com.svg" class="done"><img src="/images/done-round-green-svgrepo-com.svg" class="greenDone hidden" id="greenDone"><img src="/images/delete-1487-svgrepo-com.svg" class="trashCan"></div></div>`;
 
   tasklist.insertAdjacentHTML("beforeend", html); // bitno mi je ovdje bilo beforeend mamicu mu
   allTodos.push(addedTask);
@@ -29,21 +28,11 @@ tasklist.addEventListener("click", (event) => {
   if (event.target.classList.contains("done")) {
     const parentTask = event.target.closest(".newTask");
     if (parentTask) {
-      const doneTask = parentTask.querySelector("h4").textContent; // IME TOG FAKEN TASKA
-      doneTodos.push(doneTask);
-      parentTask.remove();
+      const greenDone = parentTask.querySelector("#greenDone");
+      greenDone.classList.remove("hidden");
+      const grayDone = parentTask.querySelector(".done");
+      grayDone.classList.add("hidden");
+      parentTask.classList.add("doneTask");
     }
   }
 });
-
-const displayDoneTasks = function () {
-  doneTasksContainer.innerHTML = "";
-  doneTodos.forEach((task) => {
-    const doneHtml = `<div class="doneTask">${task}</div>`;
-
-    doneTasksContainer.insertAdjacentHTML("beforeend", doneHtml);
-    doneTasksContainer.classList.remove("hidden");
-  });
-};
-
-buttonDoneTasks.addEventListener("click", displayDoneTasks);
